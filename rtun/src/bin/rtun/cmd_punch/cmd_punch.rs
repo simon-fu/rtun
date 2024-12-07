d9eb5f104b3cdb042bea4fd23546b40f9f65f6b3
simon-fu <simonok@139.com>
2024-12-07 12:36:15 +0800

    punch: support echo

--- rtun/src/bin/rtun/cmd_punch/cmd_punch.rs
+++ rtun/src/bin/rtun/cmd_punch/cmd_punch.rs
@@ -20,7 +20,7 @@ use tracing::{debug, info, span, Instrument, Level};
 use crate::{cmd_punch::udp_acceptor::{new_udp_reuseport_str, new_udp_reuseport_v4}, init_log_and_run};
 use rtun::ice::ice_peer::{IceArgs, IceConfig, IcePeer};
 
-use super::udp_acceptor::{UdpAcceptor, UdpBuf, UdpReceiver, UdpSender};
+use super::udp_acceptor::{new_udp_reuseport, UdpAcceptor, UdpBuf, UdpReceiver, UdpSender};
 
 
 pub fn run(args: CmdArgs) -> Result<()> { 
@@ -71,12 +71,23 @@ async fn do_run(args: CmdArgs) -> Result<()> {
     
     if let Some(listen_addr) = &args.relay_listen {
         let src = new_udp_reuseport_str(listen_addr)?;
-        info!("listen at [{listen_addr}]");
+        let addr = src.local_addr()?;
+        info!("listen at [{addr}]");
+
+        if args.echo {
+            kick_echo_client(addr).await.with_context(||"kick_echo_client failed")?;
+        }
+
         let mut tun = TunRecver::new(tun_socket.clone());
         client_side_loop(src, &mut tun).await
         
     } else if let Some(target_addr) = &args.relay_to {
         let target_addr: SocketAddr = target_addr.parse().with_context(||format!("invalid target addr [{target_addr}]"))?;
+
+        if args.echo {
+            kick_echo_server(target_addr).await.with_context(||"kick_echo_server failed")?;
+        }
+
         let mut tun = TunRecver::new(tun_socket.clone());
         server_side_loop( &mut tun, target_addr).await
 
@@ -147,6 +158,121 @@ async fn do_run(args: CmdArgs) -> Result<()> {
 
 }
 
+async fn kick_echo_server(listen_addr: SocketAddr) -> Result<()> {
+    let socket = new_udp_reuseport(listen_addr).with_context(||"bind echo server socket failed")?;
+    
+    let span = span!(parent: None, Level::DEBUG, "echo-server");
+    tokio::spawn(async move {
+        let mut buf = vec![0_u8; 1700];
+        let mut num = 0_u64;
+        loop {
+            let r = socket.recv_from(&mut buf).await;
+            let (len, from) = match r {
+                Ok(v) => v,
+                Err(e) => {
+                    tracing::warn!("recv_from error [{e:?}]");
+                    continue;
+                },
+            };
+
+            num += 1;
+            info!("recv num [{num}] from [{from}]");
+            let r = socket.send_to(&buf[..len], from).await;
+            match r {
+                Ok(_v) => {},
+                Err(e) => {
+                    tracing::warn!("No.{num}: send echo error [{e:?}] [{from}]");
+                },
+            }
+        }
+    }.instrument(span));
+
+    Ok(())
+}
+
+async fn kick_echo_client(target: SocketAddr) -> Result<()> {
+    let socket = new_udp_reuseport_v4().with_context(||"bind echo client socket failed")?;
+    socket.connect(target).await.with_context(||"echo client connect failed")?;
+    let socket = Arc::new(socket);
+
+    {
+        let span = span!(parent: None, Level::DEBUG, "echo-recv");
+        let socket = socket.clone();
+        tokio::spawn(async move {
+            echo_recv(socket).await
+        }.instrument(span));
+    }
+
+    {
+        let span = span!(parent: None, Level::DEBUG, "echo-send");
+        let socket = socket.clone();
+        tokio::spawn(async move {
+            tokio::time::sleep(Duration::from_millis(1000)).await;
+            echo_send(socket).await
+        }.instrument(span));
+    }
+
+    Ok(())
+}
+
+async fn echo_send(socket: Arc<UdpSocket>) {
+    let mut buf = BytesMut::new();
+    for n in 0..300_u64 {
+        // let content = format!("msg {n}");
+        // let r = tun_socket.send(content.as_bytes()).await.with_context(||"udp 
+        buf.put_u64(n);
+        let packet = buf.split_to(8);
+        let r = socket.send(&packet[..]).await.with_context(||"udp send failed");
+
+        match r {
+            Ok(_len) => {
+                // info!("No.{n}:  sent len [{_len}]");
+            },
+            Err(e) => {
+                tracing::warn!("No.{n}: send error [{e:?}]");
+            },
+        }
+
+        // let len = socket.send_to(content.as_bytes(), remote_addr).await.with_context(||"udp sen dto failed")?;
+        // info!("sent len [{len}]");
+
+        tokio::time::sleep(Duration::from_millis(300)).await;
+    }
+}
+
+async fn echo_recv(socket: Arc<UdpSocket>) {
+    let mut buf = vec![0_u8; 1700];
+    let mut next_seq = 0_u64;
+    loop {
+        let r = socket.recv(&mut buf).await;
+
+        let len = match r {
+            Ok(len) => len,
+            Err(e) => {
+                tracing::warn!("recv error [{e:?}]");
+                continue;
+            },
+        };
+
+        if len != 8 {
+            tracing::warn!("invalid len [{len}], seq [{next_seq}]");
+            continue;
+        }
+
+        let seq = (&buf[..8]).get_u64();
+
+        if seq != next_seq {
+            tracing::warn!("expect seq [{next_seq}], but [{seq}]");
+            if seq > next_seq {
+                next_seq = seq + 1;
+            }
+            continue;
+        }
+        info!("recv seq [{seq}] ok");
+        next_seq += 1;
+        
+    }
+}
 
 async fn client_side_loop(listen_socket: UdpSocket, tun: &mut TunRecver) -> Result<()> {
 
@@ -506,6 +632,13 @@ pub struct CmdArgs {
         long_help = "relay to targeet",
     )]
     relay_to: Option<String>,
+
+    #[clap(
+        short = 'e',
+        long = "echo",
+        long_help = "run as server",
+    )]
+    echo: bool,
 }
 
 
